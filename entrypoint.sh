a8c83cd62d9da3304ba7d4ef9b2df8da334cf6df
thejazzid <26236896+thejazzid@users.noreply.github.com>
2020-07-13 17:33:37 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 node /aws-azure-login/lib --no-sandbox "$@"
 
 if [ $? -eq 0 ]; then
-  echo "OK - will do magic here"
+  echo "Login function seems worked OK - Will try to update credentials for default profile"
   python3 /aws-azure-login/copy_creds.py "$@"
 else
-  echo "Somthing went wrong"
+  echo "Somthing went wrong with aws-azure-login function. Try again"
 fi
